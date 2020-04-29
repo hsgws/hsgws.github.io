5a6c934772ee68324acd7c705c1ab18cb38f7dee
hsgws <40028338+hsgws@users.noreply.github.com>
2020-04-29 14:28:36 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
-# comment
+# update
 apt update
+apt upgrade
+
+# Node.js
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt install nodejs
+apt install npm
+npm install pm2 -g
