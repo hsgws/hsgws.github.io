4b9c2acd6d3c7b269cc4a97c09f517dc4180bce1
hsgws <40028338+hsgws@users.noreply.github.com>
2020-09-02 01:58:06 +0900

    0902

---
+++ x7141/2020/surv.R
@@ -0,0 +1,31 @@
+library(survival)
+library(tidyverse)
+# library(rms)
+
+data_churn <- read.csv("data/data_churn.csv") 
+
+# data_churn2 <- data_churn %>% 
+#   mutate(Churn = 1*(Churn == "Yes")) %>% 
+#   filter(tenure > 0)
+
+
+result <- survreg(Surv(tenure, Churn) ~ gender + MonthlyCharges + Contract,
+                  data = data_churn)
+summary(result)
+
+
+
+
+# pred <- predict(result, type = "quantile", p = seq(0.01, 0.99, by=.01))
+
+# s <- with(data_churn, Surv(tenure, Churn))
+# km.null <- survfit(data = data_churn, s ~ 1)
+# survplot(km.null,conf ="none")
+# 
+# lines(x = predict(result, type = "quantile", p = seq(0.01, 0.99, by=.01))[1,],
+#       y = rev(seq(0.01, 0.99, by = 0.01)),
+#       col = "red",lty=2,lwd=2)
+
+# resultc <- coxph(Surv(tenure, Churn) ~ gender + MonthlyCharges + Contract,
+#                   data = data_churn)
+# summary(resultc)
