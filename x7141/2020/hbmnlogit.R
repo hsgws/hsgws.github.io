4436d8e7e7e5b4950d2907716390e320c195d630
hsgws <40028338+hsgws@users.noreply.github.com>
2020-12-01 23:14:27 +0900

    1201

--- x7141/2020/hbmnlogit.R
+++ x7141/2020/hbmnlogit.R
@@ -8,25 +8,48 @@ n <- length(id)       # 消費者パネル数
 j <- 3    # 商品の個数
 k <- 3    # マーケティング変数の個数 (Price, Disp, Ad)
 
-
-yxdata <- list()  # 各消費者の購買データを list 形式に変換
+sec8_data1[,7:9] <- log(sec8_data1[,7:9])
+yxdata <- list() 
 for (i in 1:n) {
   data_i <- subset(sec8_data1, PNL == id[i])
   ydata_i <- data_i$Choice
-  xdata_i <- createX(p = j, na = k, nd = NULL, Xa = data[,7:15], Xd = NULL, base = 3)
-  yxdata[[i]] <- list(id = id[i], y = ydata, X = xdata)
+  xdata_i <- createX(p = j, na = k, nd = NULL, Xa = data_i[,7:15], Xd = NULL, base = 3)
+  yxdata[[i]] <- list(id = id[i], y = ydata_i, X = xdata_i)
 }
 
 # zdata <- cbind(intercept = rep(1,n),
 #                scale(sec8_data2[,c("age", "family")], scale = FALSE))
 zdata <- scale(sec8_data2[,c("age", "family")], scale = FALSE)
 
-dataset <- list(p = j, lgtdata = yxdata, Z = zdata)
+dataset <- list(p = 3, lgtdata = yxdata, Z = zdata)
 
-set.seed(1234)  # 乱数の初期値
+set.seed(1)  # 乱数の初期値
 mcmc <- list(R = 3000) # MCMCのサンプル数
 prior <- list(ncomp = 1)  # 事前分布の設定 ⇒ 初期設定の散漫な事前分布
 
 # MCMCサンプリング
 out <- rhierMnlRwMixture(Data = dataset, Prior = prior, Mcmc = mcmc)    # MCMCの実行
-                    
+plot(out$betadraw)
+
+# 係数 Delta の推定値と出力整形
+summary_Delta <- summary(out$Deltadraw, QUANTILES = FALSE)
+Delta_mean <- matrix(summary_Delta[,1], nrow =5, byrow = TRUE)
+Delta_sd <- matrix(summary_Delta[,2], nrow =5, byrow = TRUE)
+
+rownames(Delta_mean) <- c("Brand.1", "Brand.2", "Price", "Disp", "Ad")
+colnames(Delta_mean) <- c("age", "family")
+rownames(Delta_sd) <- c("Brand.1", "Brand.2", "Price", "Disp", "Ad")
+colnames(Delta_sd) <- c("age", "family")
+
+Delta_mean
+Delta_sd
+
+# Delta_mean / Delta_sd
+
+# data_hblogit <- dataset
+# save(data_hblogit, file = "data_hblogit.RData")
+# load()
+
+
+save(yxdata, file = "yxdata.RData")
+
