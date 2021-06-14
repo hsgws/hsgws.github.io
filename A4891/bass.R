10ea0a7ffab16ca1a1313f8e358843feafe77a13
hsgws <40028338+hsgws@users.noreply.github.com>
2021-06-15 02:22:21 +0900

    Create bass.R

---
+++ A4891/bass.R
@@ -0,0 +1,50 @@
+data <- data[1:412,]
+nrow <- nrow(data)
+
+n <- data$positive_cases
+Nt <- c(0, cumsum(data$positive_cases[1:(nrow-1)]))
+bass_data <- data.frame(n = data$positive_cases, Nt = Nt[1:nrow], Nt2 = Nt[1:nrow]^2)
+
+
+# 1
+result1_nls <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
+                   start = list(p = 0.1, q = 0.1, m = 5000),
+                   data = bass_data[1:119,]) 
+summary(result1_nls)
+
+result1_lm <- lm(n ~ Nt + Nt2, data = bass_data[1:119,])
+summary(result1_lm)
+a <- result1_lm$coefficients[1]
+b <- result1_lm$coefficients[2]
+c <- result1_lm$coefficients[3]
+m <- (-b-sqrt(b^2-4*a*c))/(2*c)
+p <- a/m
+q <- p+b
+
+print(c(m, p, q))
+
+# library(netdiffuseR)
+fitbass(bass_data$n[120:250])
+
+ltys <- c("solid", "dashed")
+cols <- c("black", "red")
+plot.ts(cbind(bass_data$n[1:119], fitted(result1)), plot.type = "single", 
+        lty = ltys, col = cols, main = "1")
+
+# 2
+result2 <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
+               start = list(p = 0.1, q = 0.1, m = 5000),
+               data =  bass_data[120:250,]) 
+summary(result2)
+
+plot.ts(cbind(bass_data$n[120:250], fitted(result2)), plot.type = "single", 
+        lty = ltys, col = cols, main = 2)
+
+# 3
+result3 <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
+               start = list(p = 0.1, q = 0.1, m = 5000),
+               data =  bass_data[251:412,]) 
+summary(result3)
+
+plot.ts(cbind(bass_data$n[251:412], fitted(result3)), plot.type = "single", 
+        lty = ltys, col = cols, main = 3)
