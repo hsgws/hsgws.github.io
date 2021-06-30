2ada77e485605b11260dce3e454e3fc7f3d982f2
hsgws <40028338+hsgws@users.noreply.github.com>
2021-06-30 15:34:50 +0900

    Revert "Update bass.R"
    This reverts commit 7ade120c3bff87cb9bdd4febcbef8935a94dbcd8.

--- A4891/bass.R
+++ A4891/bass.R
@@ -8,15 +8,14 @@ bass_data <- data.frame(n = data$positive_cases, Nt = Nt[1:nrow], Nt2 = Nt[1:nro
 
 # 1
 ## nls
-n <- bass_data$n[1:119]
-Nt <- c(0, cumsum(n[1:(nrow(n)-1)]))
 result1_nls <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
-                   start = list(p = 0.1, q = 0.1, m = 5000)) 
+                   start = list(p = 0.1, q = 0.1, m = 5000),
+                   data = bass_data[1:119,]) 
 summary(result1_nls)
 
 ltys <- c("solid", "dashed")
 cols <- c("black", "red")
-plot.ts(cbind(n, fitted(result1_nls)), plot.type = "single", 
+plot.ts(cbind(bass_data$n[1:119], fitted(result1_nls)), plot.type = "single", 
         lty = ltys, col = cols, main = "1")
 
 ## lm
@@ -33,22 +32,20 @@ print(c(m, p, q))
 
 
 # 2
-n <- bass_data$n[120:250]
-Nt <- c(0, cumsum(n[1:(length(n)-1)]))
 result2 <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
-               start = list(p = 0.1, q = 0.1, m = 5000)) 
+               start = list(p = 0.1, q = 0.1, m = 5000),
+               data =  bass_data[120:250,]) 
 summary(result2)
 
-plot.ts(cbind(n, fitted(result2)), plot.type = "single", 
+plot.ts(cbind(bass_data$n[120:250], fitted(result2)), plot.type = "single", 
         lty = ltys, col = cols, main = 2)
 
 
 # 3
-n <- bass_data$n[251:412]
-Nt <- c(0, cumsum(n[1:(length(n)-1)]))
 result3 <- nls(n ~ p*(m - Nt) + q*Nt/m*(m - Nt),
-               start = list(p = 0.1, q = 0.1, m = 5000)) 
+               start = list(p = 0.1, q = 0.1, m = 5000),
+               data =  bass_data[251:412,]) 
 summary(result3)
 
-plot.ts(cbind(n, fitted(result3)), plot.type = "single", 
+plot.ts(cbind(bass_data$n[251:412], fitted(result3)), plot.type = "single", 
         lty = ltys, col = cols, main = 3)
