b248e41385ea21161580168bc3977979d3c2a8a3
hsgws <40028338+hsgws@users.noreply.github.com>
2021-07-07 22:25:21 +0900

    Update sem.R

--- A4891/sem.R
+++ A4891/sem.R
@@ -14,4 +14,17 @@ summary(fit, fit.measures = TRUE, standardized = TRUE)
 
 
 library(sem)
-model.sem <- specifyEquations(covs = )
+#model.sem <- specifyEquations(covs = )
+
+library(lavaan)
+
+model <- "
+  tech =~ After_Sales_Service + Technology + Space_comfort + Safety + Fuel_Type
+  price =~ Price + Resale_Value + Fuel_Efficiency + Maintenance
+  ride =~ Exterior_Looks + Test_drive + Product_reviews + Testimonials
+  price ~ tech
+  ride ~ tech + price
+"
+
+fit <- sem(model, data = data_car)
+summary(fit, fit.measures = TRUE, standardized = TRUE)
